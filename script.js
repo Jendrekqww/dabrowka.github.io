ccfcb5476833ab1c9a8ffa5bd732efa89f53ea85
Jendrekqww <97966983+Jendrekqww@users.noreply.github.com>
2024-05-27 19:46:14 +0200

    ye
    ye

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-let Monday =[9,'Cloudy',50,6.2]
-let Tuesday=[11,'Sunny ',96,89,85]
-let Wensday=[16,'Rainy',99,79,98]
-let Thursday=[13,'Windless',10,13,9]
-let Friday=[12,'Foggy',10,13,9]
 function on(){
-    var t = document.getElementById('day').value
-    switch (t) {
-        case 'Monday':
-          out(Monday)
-          break;
-        case 'Tuesday':
-          out(Tuesday)
-          break;
-      case 'Wensday':
-          out(Wensday)
-          break;
-          case 'Thursday':
-              out(Thursday)
-              break;
-              case 'Friday':
-                out(Friday)
-                break;
-        
-              
-      }
+    let logrex = /[a-z]@gmail.com/;
+
+    let log = document.getElementById('login').value
+    let password = document.getElementById('password').value
     
-}
+let silverRegex = /@gmail.com/;
 
-function out(arr){
+    if(log.match(logrex)!= null && password.length>8){
+        
+            location.href = "in.html"; 
 
-    document.getElementById('temp').innerHTML =arr[0]+"°C"
-    document.getElementById('des').innerHTML =arr[1]
-    document.getElementById('hum').innerHTML ="Humidity "+arr[2] +"%"
-    document.getElementById('wind').innerHTML ="Wind speed "+arr[3]+"km/h"
+    }
+    else if(log.match(logrex)== null && password.length>8){
+        alert('zły email')
+    }
+    else if(log.match(logrex)!= null && password.length<=8){
+        alert('Za krótkie hasło')
+    }
+    else{
+        alert('Złe hasło i email')
+    }
+
+    var name = log.replace(silverRegex, "")
+
+    document.querySelector('.name').innerHTML = 'chuj'
+    
+
 }
+    
